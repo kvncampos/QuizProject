07c7de0454c45e0064171cfe6ae57bbac34b9419
Kevin Campos <kvnelpdfw@hotmail.com>
2022-07-09 18:19:19 -0500

    Fixes some issues.

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
 from question_model import Question
-from data import question_data, json_list
+from data import json_list
 from quiz_brain import QuizBrain
 
 open_trivia_bank = []
-question_bank = []
-
-for question in question_data:
-    q_text = question["text"]
-    q_answer = question["answer"]
-    new_question = Question(q_text, q_answer)
-    question_bank.append(new_question)
 
 for key in json_list:
     q_text = key["question"]
